46e5624d56bbc7531c6ed9ed669e062acaec69c0
Корнеев Владимир <65484013+VollyKor@users.noreply.github.com>
2021-05-18 15:52:47 +0300

    Update NavBar.ts

--- src/styled/NavBar.ts
+++ src/styled/NavBar.ts
@@ -2,11 +2,12 @@ import styled from 'styled-components';
 import { Nav } from 'react-bootstrap';
 
 export const Navbar = styled(Nav)`
+    justify-content: center;
+    
     background: var(--white-color);
     border-bottom: 1px solid var(--logo-color);
+    
     text-align: center;
-
-    justify-content: center;
 
     @media ${(props) => props.theme?.deviceSize?.small} {
         justify-content: space-around;
